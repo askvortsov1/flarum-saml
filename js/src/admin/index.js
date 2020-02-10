f545040203ead3a4dfef6febe3ad071b4372396e
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-02-10 15:34:53 -0500

    Added setting to control attribute sync

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -13,18 +13,15 @@ app.initializers.add('askvortsov/saml', () => {
                 title: app.translator.trans('askvortsov-saml.admin.title'),
                 type: 'small',
                 items: [
-                    // IdP Config
                     <StringItem key="askvortsov-saml.idp_metadata" required>
                         {app.translator.trans('askvortsov-saml.admin.labels.idp_metadata')}
                     </StringItem>,
-                    // General Info
                     <BooleanItem key="askvortsov-saml.only_option" required>
                         {app.translator.trans('askvortsov-saml.admin.labels.only_option')}
                     </BooleanItem>,
-                    // Group Management
-                    // <BooleanItem key="askvortsov-saml.sync_attributes" required>
-                    //     {app.translator.trans('askvortsov-saml.admin.labels.sync_attributes')}
-                    // </BooleanItem>,
+                    <BooleanItem key="askvortsov-saml.sync_attributes" required>
+                        {app.translator.trans('askvortsov-saml.admin.labels.sync_attributes')}
+                    </BooleanItem>,
 
                 ],
             })
