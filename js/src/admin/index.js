6f57a489569422e6278527ce10cbcd7cf37a80c9
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-25 23:33:48 -0400

    Add JS linting / build config

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,46 +1,66 @@
-import { settings } from '@fof-components';
+import { settings } from "@fof-components";
 
 const {
-    SettingsModal,
-    items: { BooleanItem, SelectItem, StringItem },
+  SettingsModal,
+  items: { BooleanItem, SelectItem, StringItem },
 } = settings;
 
+app.initializers.add("askvortsov/saml", () => {
+  app.extensionSettings["askvortsov-saml"] = () =>
+    app.modal.show(
+      new SettingsModal({
+        title: app.translator.trans("askvortsov-saml.admin.title"),
+        type: "small",
+        items: [
+          <p>
+            Make sure that either the metadata url or the metadata is filled in.
+          </p>,
+          <StringItem
+            key="askvortsov-saml.idp_metadata_url"
+            id="askvortsov-saml-metadata_url"
+          >
+            {app.translator.trans(
+              "askvortsov-saml.admin.labels.idp_metadata_url"
+            )}
+          </StringItem>,
+          <StringItem
+            key="askvortsov-saml.idp_metadata"
+            id="askvortsov-saml-metadata"
+          >
+            {app.translator.trans("askvortsov-saml.admin.labels.idp_metadata")}
+          </StringItem>,
+          <BooleanItem key="askvortsov-saml.only_option" required>
+            {app.translator.trans("askvortsov-saml.admin.labels.only_option")}
+          </BooleanItem>,
+          <BooleanItem key="askvortsov-saml.sync_attributes" required>
+            {app.translator.trans(
+              "askvortsov-saml.admin.labels.sync_attributes"
+            )}
+          </BooleanItem>,
+          <div className="Form-group">
+            <label>
+              {app.translator.trans(
+                "askvortsov-saml.admin.labels.nameid_format"
+              )}
+            </label>
 
-app.initializers.add('askvortsov/saml', () => {
-    app.extensionSettings['askvortsov-saml'] = () =>
-        app.modal.show(
-            new SettingsModal({
-                title: app.translator.trans('askvortsov-saml.admin.title'),
-                type: 'small',
-                items: [
-                    <p>Make sure that either the metadata url or the metadata is filled in.</p>,
-                    <StringItem key="askvortsov-saml.idp_metadata_url" id="askvortsov-saml-metadata_url">
-                        {app.translator.trans('askvortsov-saml.admin.labels.idp_metadata_url')}
-                    </StringItem>,
-                    <StringItem key="askvortsov-saml.idp_metadata" id="askvortsov-saml-metadata">
-                        {app.translator.trans('askvortsov-saml.admin.labels.idp_metadata')}
-                    </StringItem>,
-                    <BooleanItem key="askvortsov-saml.only_option" required>
-                        {app.translator.trans('askvortsov-saml.admin.labels.only_option')}
-                    </BooleanItem>,
-                    <BooleanItem key="askvortsov-saml.sync_attributes" required>
-                        {app.translator.trans('askvortsov-saml.admin.labels.sync_attributes')}
-                    </BooleanItem>,
-                    <div className="Form-group">
-                        <label>{app.translator.trans('askvortsov-saml.admin.labels.nameid_format')}</label>
-
-                        {SelectItem.component({
-                            options: {
-                                "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent": app.translator.trans('askvortsov-saml.admin.options.nameid_format.persistent'),
-                                "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress": app.translator.trans('askvortsov-saml.admin.options.nameid_format.emailAddress'),
-                                "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified": app.translator.trans('askvortsov-saml.admin.options.nameid_format.unspecified')
-
-                            },
-                            key: 'askvortsov-saml.nameid_format',
-                            required: true,
-                        })}
-                    </div>,
-                ],
-            })
-        );
+            {SelectItem.component({
+              options: {
+                "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent": app.translator.trans(
+                  "askvortsov-saml.admin.options.nameid_format.persistent"
+                ),
+                "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress": app.translator.trans(
+                  "askvortsov-saml.admin.options.nameid_format.emailAddress"
+                ),
+                "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified": app.translator.trans(
+                  "askvortsov-saml.admin.options.nameid_format.unspecified"
+                ),
+              },
+              key: "askvortsov-saml.nameid_format",
+              required: true,
+            })}
+          </div>,
+        ],
+      })
+    );
 });
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,101 +1,114 @@
-import { extend, override } from 'flarum/extend';
-import app from 'flarum/app';
+import { extend, override } from "flarum/extend";
+import app from "flarum/app";
 import HeaderSecondary from "flarum/components/HeaderSecondary";
-import LogInButton from 'flarum/components/LogInButton';
-import LogInButtons from 'flarum/components/LogInButtons';
+import LogInButton from "flarum/components/LogInButton";
+import LogInButtons from "flarum/components/LogInButtons";
 import LogInModal from "flarum/components/LogInModal";
 import SettingsPage from "flarum/components/SettingsPage";
 import SignUpModal from "flarum/components/SignUpModal";
 
-app.initializers.add('askvortsov/saml', () => {
-    override(LogInModal.prototype, 'body', dontShowLoginModalIfOnlySaml);
-    override(SignUpModal.prototype, 'body', dontShowSignupModalIfOnlySaml);
-    override(SignUpModal.prototype, 'title', clarifySignupModalTitleAfterSaml);
-    extend(LogInButtons.prototype, 'items', addSamlLoginButton);
+app.initializers.add("askvortsov/saml", () => {
+  override(LogInModal.prototype, "body", dontShowLoginModalIfOnlySaml);
+  override(SignUpModal.prototype, "body", dontShowSignupModalIfOnlySaml);
+  override(SignUpModal.prototype, "title", clarifySignupModalTitleAfterSaml);
+  extend(LogInButtons.prototype, "items", addSamlLoginButton);
 
-    extend(SettingsPage.prototype, 'accountItems', removeProfileActions);
-    extend(SettingsPage.prototype, 'settingsItems', checkRemoveAccountSection);
+  extend(SettingsPage.prototype, "accountItems", removeProfileActions);
+  extend(SettingsPage.prototype, "settingsItems", checkRemoveAccountSection);
 
-
-    function showSamlPopup(e) {
-        if (app.forum.attribute('onlyUseSaml')) {
-            var win = window.open("/auth/saml/login", "_blank", "height=500,width=600,resizable=no,toolbar=no,menubar=no,location=no,status=no")
-            if (win == null) {
-                win = window.open("/auth/saml/login", "_blank");
-            }
-            if (win == null) {
-                alert(app.translator.trans('askvortsov-saml.forum.log_in.enable_popups'))
-            }
-            win.focus();
-        }
-    }
-
-    function seePopupText() {
-        return [
-            <a class="Button Button--primary" href="/auth/saml/login" target="_blank">{app.translator.trans('askvortsov-saml.forum.log_in.open_popup')}</a>
-        ]
-    }
-
-
-    function dontShowLoginModalIfOnlySaml() {
-        if (app.forum.attribute('onlyUseSaml')) {
-            return seePopupText();
-        } else {
-            return [
-                <LogInButtons />,
-
-                <div className="Form Form--centered">
-                    {this.fields().toArray()}
-                </div>
-            ];
-        }
-    }
-
-    function clarifySignupModalTitleAfterSaml() {
-        if (!this.props.token) {
-            return app.translator.trans('core.forum.sign_up.title');
-        }
-
-        return app.translator.trans('askvortsov-saml.forum.sign_up.post_saml_title');
-    }
-
-    function dontShowSignupModalIfOnlySaml() {
-        if (app.forum.attribute('onlyUseSaml') && (jQuery.isEmptyObject(this.props) || this.props.username == "" && this.props.password == "")) {
-            return seePopupText();
-        } else {
-            return [
-                this.props.token ? '' : <LogInButtons />,
-
-                <div className="Form Form--centered">
-                    {this.fields().toArray()}
-                </div>
-            ];
-        }
-    }
-
-    function addSamlLoginButton(items) {
-        items.add('saml',
-            <LogInButton
-                className="Button LogInButton--saml"
-                icon="fas fa-lock"
-                path="/auth/saml/login">
-                {app.translator.trans('askvortsov-saml.forum.log_in.with_saml_button')}
-            </LogInButton>
+  function showSamlPopup(e) {
+    if (app.forum.attribute("onlyUseSaml")) {
+      var win = window.open(
+        "/auth/saml/login",
+        "_blank",
+        "height=500,width=600,resizable=no,toolbar=no,menubar=no,location=no,status=no"
+      );
+      if (win == null) {
+        win = window.open("/auth/saml/login", "_blank");
+      }
+      if (win == null) {
+        alert(
+          app.translator.trans("askvortsov-saml.forum.log_in.enable_popups")
         );
-    };
-
-    function removeProfileActions(items) {
-        items.remove('changeEmail');
-        items.remove('changePassword');
+      }
+      win.focus();
     }
-    function checkRemoveAccountSection(items) {
-        if (items.has('account') &&
-            items.get('account').props.children.length === 0) {
-            items.remove('account');
-        }
+  }
+
+  function seePopupText() {
+    return [
+      <a class="Button Button--primary" href="/auth/saml/login" target="_blank">
+        {app.translator.trans("askvortsov-saml.forum.log_in.open_popup")}
+      </a>,
+    ];
+  }
+
+  function dontShowLoginModalIfOnlySaml() {
+    if (app.forum.attribute("onlyUseSaml")) {
+      return seePopupText();
+    } else {
+      return [
+        <LogInButtons />,
+
+        <div className="Form Form--centered">{this.fields().toArray()}</div>,
+      ];
+    }
+  }
+
+  function clarifySignupModalTitleAfterSaml() {
+    if (!this.props.token) {
+      return app.translator.trans("core.forum.sign_up.title");
     }
 
-    $(function () {
-        $('.item-logIn>button,.item-signUp>button').on("click", showSamlPopup);
-    });
+    return app.translator.trans(
+      "askvortsov-saml.forum.sign_up.post_saml_title"
+    );
+  }
+
+  function dontShowSignupModalIfOnlySaml() {
+    if (
+      app.forum.attribute("onlyUseSaml") &&
+      (jQuery.isEmptyObject(this.props) ||
+        (this.props.username == "" && this.props.password == ""))
+    ) {
+      return seePopupText();
+    } else {
+      return [
+        this.props.token ? "" : <LogInButtons />,
+
+        <div className="Form Form--centered">{this.fields().toArray()}</div>,
+      ];
+    }
+  }
+
+  function addSamlLoginButton(items) {
+    items.add(
+      "saml",
+      <LogInButton
+        className="Button LogInButton--saml"
+        icon="fas fa-lock"
+        path="/auth/saml/login"
+      >
+        {app.translator.trans("askvortsov-saml.forum.log_in.with_saml_button")}
+      </LogInButton>
+    );
+  }
+
+  function removeProfileActions(items) {
+    items.remove("changeEmail");
+    items.remove("changePassword");
+  }
+
+  function checkRemoveAccountSection(items) {
+    if (
+      items.has("account") &&
+      items.get("account").props.children.length === 0
+    ) {
+      items.remove("account");
+    }
+  }
+
+  $(function () {
+    $(".item-logIn>button,.item-signUp>button").on("click", showSamlPopup);
+  });
 });
