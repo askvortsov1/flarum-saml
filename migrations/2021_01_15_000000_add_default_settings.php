14700d9021685549d04c62e9274866a3d0bbeec9
Alexander Skvortsov <askvortsov1@users.noreply.github.com>
2021-01-15 20:15:22 +0000

    Apply fixes from StyleCI

--- migrations/2021_01_15_000000_add_default_settings.php
+++ migrations/2021_01_15_000000_add_default_settings.php
@@ -1,9 +1,18 @@
 <?php
 
+/*
+ * This file is part of askvortsov/flarum-saml
+ *
+ *  Copyright (c) 2020 Alexander Skvortsov.
+ *
+ *  For detailed copyright and license information, please view the
+ *  LICENSE file that was distributed with this source code.
+ */
+
 use Flarum\Database\Migration;
 
 return Migration::addSettings([
     'askvortsov-saml.want_assertions_signed' => true,
-    'askvortsov-saml.want_messages_signed' => true,
-    'askvortsov-saml.nameid_format' => 'askvortsov-saml.admin.options.nameid_format.emailAddress'
+    'askvortsov-saml.want_messages_signed'   => true,
+    'askvortsov-saml.nameid_format'          => 'askvortsov-saml.admin.options.nameid_format.emailAddress',
 ]);
